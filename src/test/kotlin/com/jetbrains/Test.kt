d454653db863badd9443bcfbccbdd37848fdd353
Nikita Salomatin <nsalomatin@hotmail.com>
2021-04-21 18:57:53 +0300

    KTIJ-10020 reproduce

---
+++ src/test/kotlin/com/jetbrains/Test.kt
@@ -0,0 +1,11 @@
+package com.jetbrains
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class Test {
+    @Test
+    fun somethingWrong() {
+        assertEquals(true, false)
+    }
+}
